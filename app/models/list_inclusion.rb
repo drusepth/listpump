c017a0dc05dd3c20b3c0301c0f68bfdc252acbd9
Andrew Brown <drusepth@gmail.com>
2019-08-28 01:41:50 -0500

    time machine

--- app/models/list_inclusion.rb
+++ app/models/list_inclusion.rb
@@ -3,5 +3,5 @@ class ListInclusion < ApplicationRecord
   belongs_to :person
   belongs_to :trigger
 
-  default_scope where(flagged_invalid_at: nil)
+  default_scope { where(flagged_invalid_at: nil) }
 end

0fdc151e9f77dcaec52563c21c39fccf9c2f96f5
Andrew Brown <drusepth@gmail.com>
2019-08-28 01:22:44 -0500

    hmmmm

--- db/migrate/20190317235153_create_familial_list_relationships.rb
+++ db/migrate/20190317235153_create_familial_list_relationships.rb
@@ -1,8 +1,8 @@
 class CreateFamilialListRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :familial_list_relationships do |t|
-      t.references :parent_list, foreign_key: true
-      t.references :child_list, foreign_key: true
+      t.int :parent_list_id, foreign_key: true
+      t.int :child_list_id, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20190828060815_fake_migration_lol.rb
+++
@@ -1,4 +0,0 @@
-class FakeMigrationLol < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
